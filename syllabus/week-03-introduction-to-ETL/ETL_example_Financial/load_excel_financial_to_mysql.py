cae024517207f71139b23a29a21ae74619d134c1
Mahmoud Parsian <mahmoud.parsian@yahoo.com>
2025-04-10 11:53:03 -0700

    updated

--- syllabus/week-03-introduction-to-ETL/ETL_example_Financial/load_excel_financial_to_mysql.py
+++ syllabus/week-03-introduction-to-ETL/ETL_example_Financial/load_excel_financial_to_mysql.py
@@ -34,42 +34,71 @@ Replace username, password, host, port, database with
 your corresponding values.
 """
 
+import sys
+import json
+import pandas as pd
 from sqlalchemy import create_engine
 
-# engine = create_engine("mysql+pymysql://" + "username" + ":" + "password" + "@" + "host" + ":" + "port" + "/" + "database" + "?" + "charset=utf8mb4")
-db_user = "root"
-db_password = "mp22pass"
-db_name = "test"
-engine = create_engine("mysql+pymysql://" + db_user + ":" + db_password + "@" + "localhost" + ":" + "3306" + "/" + db_name + "?" + "charset=utf8mb4")
+#-----------------------------------------
+# Database config is given as a JSON file
+# read a JSON file and return a dictionary
+#
+def read_json(json_config_file):
+    with open(json_config_file) as f:
+        # Load the JSON data into a Python dictionary
+        config_as_dict = json.load(f)
+        return config_as_dict
+#end-def
+#-----------------------------------------
+#
+def create_db_connection(db_config):
+    # engine = create_engine("mysql+pymysql://" + "username" + ":" + "password" + "@" + "host" + ":" + "port" + "/" + "database" + "?" + "charset=utf8mb4")
+    #
+    db_user = db_config["user"]
+    db_password = db_config["password"]
+    db_name = db_config["database"]
+    db_host = db_config["host"]
+    #
+    engine = create_engine("mysql+pymysql://" + db_user + ":" + db_password + "@" + db_host + ":" + "3306" + "/" + db_name + "?" + "charset=utf8mb4")
+    # After creating the instance, now you 
+    # should establish a connection to the 
+    # database using connect().    
+    return engine.connect();
+#
+#end-def
+#-------------------------------------------------
+# Python main driver program ....
+#-------------------------------------------------
+# Command Line Parameter 1: 
+# excel_file_name = "data_financial_sample.xlsx"
+#-------------------------------------------------
+excel_file_name = sys.argv[1]
+# excel_file_name = "data_financial_sample.xlsx"
+print("excel_file_name=", excel_file_name)
 
-# After creating the instance, now you 
-# should establish a connection to the 
-# database using connect().
 
-conn = engine.connect()
+#-------------------------------------------------
+# Command Line Parameter 2: "db_config.json"
+#-------------------------------------------------
+# This is for a "Star Schema" Database
+# db_config_file = "db_config.json"
+db_config_file = sys.argv[2]
 
-# Now that we have our connection established, 
-# it is time to work a little with Pandas.
+db_config = read_json(db_config_file)
+print("db_config=", db_config)
 
-# Reading an excel file using Pandas
-# First create an excel file obj using 
-# pandas ExcelFile() and pass your filename inside.
 
-import pandas as pd
-import sys
-
-# excel_file_name = sys.argv[1]
-excel_file_name = "financial_sample.xlsx"
-print("excel_file=", excel_file_name)
 
 # create Excel file as an excel_file object
 excel_file = pd.ExcelFile(excel_file_name)
+print("excel_file=", excel_file)
 
 # After creating the excel file obj, now it's 
 # time to parse the excel sheet using parse() function. 
 # Pass the sheet name that you want to process.
 
 excel_dataframe = excel_file.parse(sheet_name="Sheet1")
+#print("excel_dataframe=", excel_dataframe)
 
 # And yes! The data has been copied to a dataframe.
 # Let's move on to the last step where we load this 
@@ -86,6 +115,13 @@ print("excel_dataframe=",excel_dataframe)
 # The parameter if_exists checks if the table 
 # already exists in the database. If yes, in 
 # this case it will append the data.
+
+# After creating the instance, now you 
+# should establish a connection to the 
+# database using connect().
+
+conn = create_db_connection(db_config)
+print("conn=", conn)
 
 excel_dataframe.to_sql("financial_table", conn, if_exists="append")
 
